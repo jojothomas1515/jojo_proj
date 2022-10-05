2c048a1111fb6ddd328a361eb1acf19b77db0a20
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-04 23:46:59 -0700

    now my default editor is emacs

---
+++ c/myusefulfunc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myusefulfunc.h"
+
+void printName(char *firstname, char *lastname)
+{
+  printf("My fullname is %s %s \n", firstname, lastname);
+  printf("ha ha aha ha ha ah ah \n");
+}
