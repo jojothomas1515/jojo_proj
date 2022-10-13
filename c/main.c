ac6f1dd49345e364382fc235cd0815a6f5b6f87f
Jojo Thomas <jojothomas1515@gmail.com>
2022-10-13 08:59:54 -0700

    cisfun

--- c/main.c
+++ c/main.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include "myusefulfunc.h"
 
+int addi(int n, int m)
+{
+	return n+m;
+}
+
 int main(){
    printName("jojo", "thomas");
-  
-  for (int i =0; i<30000; i++){
-    if((i%10000) == 0){
-      printf("%d \n", (i/10000));
-    }}
-  int count;
-  printf("Enter a number \n");
-  scanf("%d", &count);
-  for(int i=0; i<count; i++){
-    printf("%d ", i);
-    if ((i%50) == 0){
-      printf("\n newline \n");
-    }
-  }
-  return 0;
+   printf("\n%d\n" , addi(22,33));
+   printf("\a hello how are you");
+   return 0;
 }
---
+++ c/main.s
@@ -0,0 +1,83 @@
+	.file	"main.c"
+	.text
+	.globl	addi
+	.type	addi, @function
+addi:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	addi, .-addi
+	.section	.rodata
+.LC0:
+	.string	"thomas"
+.LC1:
+	.string	"jojo"
+.LC2:
+	.string	"\n%d\n"
+.LC3:
+	.string	"\007 hello how are you"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rsi
+	leaq	.LC1(%rip), %rdi
+	call	printName@PLT
+	movl	$33, %esi
+	movl	$22, %edi
+	call	addi
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
